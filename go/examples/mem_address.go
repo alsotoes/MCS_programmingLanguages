d84744438dc972c7b4afa54a5965759a0cc6b6d4
alsotoes <alsotoes@gmail.com>
2017-04-01 13:42:27 -0600

    Some more examples of concurrent code and others

---
+++ go/examples/mem_address.go
@@ -0,0 +1,26 @@
+/*
+
+	RUN: env GODEBUG=gctrace=1 go run mem_address.go
+
+*/
+
+package main
+
+import "fmt"
+
+func plus(a int, b int) int {
+    return a + b
+}
+
+func plusPlus(a, b, c int) int {
+    return a + b + c
+}
+
+func main() {
+    funcp := plus
+    funcpp := plusPlus
+    fmt.Println(funcp)
+    fmt.Println(funcpp)
+    fmt.Println(funcp(1, 2))
+    fmt.Println(funcpp(1, 2, 3))
+}
